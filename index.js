f0cf2909b07c7cdc5f6eff4f30285636a857cd00
Chronogis <denis.salon@hotmail.fr>
2017-05-27 21:51:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (/alex/i.test(message.content))
     {
-        message.channel.send(`${user.name = Meteor, id = 5270} est con`, {tts: false});
+        message.channel.send(`${username = 'Meteor', id = 5270} est con`, {tts: false});
     }
 });
 
